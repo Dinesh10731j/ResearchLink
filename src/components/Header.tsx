c6b5c2c33814d7116afe85793e7ae6a10ed7406a
Dinesh10731j <dinesh1122@gmail.com>
2024-07-22 14:46:35 +0545

    initializing

---
+++ src/components/Header.tsx
@@ -0,0 +1,43 @@
+import { motion } from "framer-motion";
+const Header = () => {
+  return (
+    <header className="flex justify-between items-center shadow-xl py-3 md:py-5 px-20 md:px-36 font-serif ">
+      <section>
+        <motion.h1
+          className="text-green-400 md:text-3xl  text-xl"
+          initial={{ opacity: 0, y: -80 }}
+          animate={{opacity:1,y:0}}
+          transition={{type:'spring',stiffness:300}}
+        >
+          ResearchLink
+        </motion.h1>
+      </section>
+
+      <section className="flex gap-3">
+        <section>
+          <motion.button className="hover:border-b hover:border-b-gray-500"
+          
+          initial={{ opacity: 0, y: -80 }}
+          animate={{opacity:1,y:0}}
+          transition={{type:'spring',stiffness:300}}
+          >
+            Login
+          </motion.button>
+        </section>
+
+        <section>
+          <motion.button className="text-green-400 hover:border-b hover:border-b-green-400"
+          
+          initial={{ opacity: 0, y: -80 }}
+          animate={{opacity:1,y:0}}
+          transition={{type:'spring',stiffness:300}}
+          >
+            Login for free
+          </motion.button>
+        </section>
+      </section>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/landingpage/Home.tsx
@@ -0,0 +1,74 @@
+import Header from "../components/Header"
+import Reseacher from "../assets/researcher.jpg";
+import ResearchMaterial from "../assets/rsearchmaterial.jpg";
+import {motion} from "framer-motion"
+const Home = () => {
+  return (
+
+    <>
+
+<Header/>
+
+<section className="flex flex-col  md:gap-7 justify-center items-center py-3 px-12 md:flex-row">
+
+
+  <section className="bg-slate-200 md:py-36 md:px-12 py-7 px-4">
+    <motion.h1 className="md:text-5xl font-serif font-medium text-3xl"
+     initial={{ opacity: 0, y: -80 }}
+     animate={{opacity:1,y:0}}
+     transition={{type:'spring',stiffness:300}}
+    >
+      Discover scientific knowledge and stay connected to the world of science</motion.h1>
+    <motion.button className="md:py-5 px-12 py-3 bg-green-300 text-white rounded-md text-xl mt-12"
+    
+    initial={{ opacity: 0, y: -80 }}
+    animate={{opacity:1,y:0}}
+    transition={{type:'spring',stiffness:300}}
+    >Join for free</motion.button>
+  </section>
+<section className="py-36 px-7">
+  <motion.img src={Reseacher} alt="research-image" className="h-[500px] w-[700px]" initial={{opacity:0 ,y:-20}}
+  
+  animate={{opacity:1,y:0}}
+
+  transition={{type:'spring',stiffness:100}}
+  />
+</section>
+</section>
+
+
+<section className="flex  flex-col gap-3 md:flex-row justify-center items-center ">
+  <motion.section className="h-96 w-96 bg-slate-200 rounded-full shadow-md"
+   initial={{ opacity: 0, y: -80 }}
+   animate={{opacity:1,y:0}}
+   transition={{type:'spring',stiffness:300}}
+  >
+    <img src={ResearchMaterial} alt="research-material" className=" h-96 w-96 rounded-full"
+    
+    
+    />
+   
+  </motion.section>
+
+
+  <section className="py-2 px-20">
+
+<motion.h1 className="text-2xl  md:text-5xl font-medium font-serif "
+
+initial={{ opacity: 0, y: -80 }}
+animate={{opacity:1,y:0}}
+transition={{type:'spring',stiffness:300}}
+>Discover research</motion.h1>
+<motion.h1 className="text-xl md:text-2xl"  initial={{ opacity: 0, y: -80 }}
+     animate={{opacity:1,y:0}}
+     transition={{type:'spring',stiffness:300}}>Access over 160 million publication pages 
+  and stay up to date with what's happening in your field.</motion.h1>
+
+  </section>
+</section>
+    </>
+  
+  )
+}
+
+export default Home
