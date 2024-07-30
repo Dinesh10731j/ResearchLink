1b66234e60382ed90d3f91bad5a99aa277ce5d10
Dinesh10731j <dinesh1122@gmail.com>
2024-07-30 17:11:34 +0545

    getfriendrequest endpoint is intregated and  send request functionalities is added

--- src/Endpoints/endpoints.ts
+++ src/Endpoints/endpoints.ts
@@ -2,10 +2,10 @@ export const endpoints = {
     UserSignup:'/api/auth/signup',
     UserLogin:'/api/auth/login',
     Uploadpaper:'/api/upload-paper',
-    Getprofileimage:'/api/profile',
     Uploadimage:'/api/upload-image',
 Userdetails:'/api/user-details',
 Researchpaper:'/api/research-papers',
 Researchlinkusers:'/api/users',
-Sendrequest:'/api/send-request'
+Sendrequest:'/api/send-request',
+Getrequest:'/api/get-request'
 }
